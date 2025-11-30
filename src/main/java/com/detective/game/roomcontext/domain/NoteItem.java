d5acc6b692d3e790841e6169aa9e18e823d8f3b1
SooHyeon Lee <dalsu4594@naver.com>
2025-11-30 20:00:16 +0900

    refactor: AI한테 보내는 데이터 수정
    - 이름이 아니라 아이디로 줘야 AI가 판단함.

--- src/main/java/com/detective/game/roomcontext/domain/NoteItem.java
+++ src/main/java/com/detective/game/roomcontext/domain/NoteItem.java
@@ -7,9 +7,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class NoteItem {
-    private final String noteName;  //"A1", "B3"
+    private final String noteName;
     private final boolean heSpy;    // isHeSpy
-    private final String itemId;
+    private final String itemId;    //"A1", "B3"
 
     public static NoteItem of(String noteName, boolean heSpy, String itemId) {
         return new NoteItem(noteName, heSpy, itemId);
